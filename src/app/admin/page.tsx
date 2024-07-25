34fbf0de4a96f0115ee94e86f559e722ecb90c8a
ml-vines <ml.vines.93@gmail.com>
2024-07-25 20:21:57 -0300

    insert user in db

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,7 @@
+import { insertUser, queryUsers } from "@/lib/db"
+
 export default function Teste(){
+    insertUser('José da silva', 'joseDaSilva@email.com', 'senha12313124')
+    // queryUsers()
     return <h1>ola</h1>
 }
---
+++ src/lib/db.ts
@@ -0,0 +1,33 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+export const conn = new Pool({
+  user: "default",
+  password: "N3wXpgJS2tHl",
+  host: "ep-rough-morning-a4giuifp.us-east-1.aws.neon.tech",
+  port: 5432,
+  database: "verceldb",
+  ssl: true,
+});
+
+export async function queryUsers() {
+  const res = await conn.query("SELECT * FROM users");
+
+  // console.log("query", res);
+}
+
+export async function insertUser(name: string, email: string, password: string) {
+  if (!name) return {
+    message: 'Nome obrigatório'
+  }
+  if (!email) return {
+    message: 'Email obrigatório'
+  }
+  if (!password) return {
+    message: 'Senha obrigatória'
+  }
+  const res = await conn.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3)', [name, email, password]);
+
+  console.log('insert', res)
+}
