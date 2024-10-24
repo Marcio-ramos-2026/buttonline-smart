ecc2cf4662e22f26197b14d9e789b93cb5539676
ml-vines <ml.vines.93@gmail.com>
2024-10-24 18:46:10 -0300

    add new sidebar to admin route

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import * as React from "react"
+import {
+  FileText,
+  UserRound,
+  UsersRound,
+} from "lucide-react"
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Usuários",
+      url: "#",
+      icon: UserRound
+    },
+    {
+      title: "Clientes",
+      url: "#",
+      icon: UsersRound
+    },
+    {
+      title: "Pedidos",
+      url: "#",
+      icon: FileText
+    }
+  ]
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        {/*imagem logo*/}
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} title='Teste' />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
