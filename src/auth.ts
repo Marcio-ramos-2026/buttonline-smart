97a2335cd9f209c8d33d67d4c992a8ae00175c0e
Vinicius  Rangel <ml.vines.93@gmail.com>
2024-11-11 21:23:30 -0300

    prisma auth structure

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,10 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { findUserByEmail } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import {prisma} from '@/lib/prisma'
+import type { Adapter } from 'next-auth/adapters';
 
-declare module "next-auth" {
-  interface Session {
-    user: {
-      email: string;
-      name: string;
-      id: number;
-    };
-  }
-}
 
 type UserType = {
   email: string;
@@ -19,33 +13,50 @@ type UserType = {
   id: number;
 };
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      //@ts-ignore
-      authorize: async (credentials) => {
-        let user: UserType;
-
-        try {
-          user = await findUserByEmail(credentials.email as string);
-        } catch (e) {
-          console.log("error", e);
-          throw new Error("Erro interno");
-        }
-
-        if (!user) throw new Error("Usuário ou senha inválidos.");
-
-        if (credentials.password !== user.password) throw new Error("Usuário ou senha inválidos.");
-
-        return user;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  session: {
+    strategy: "database", // Ensures the session uses database strategy
   },
-});
+  providers: [],
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    session({ session, user }) {
+      session.user.role = user.role
+      return session
+    }
+  }
+})
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [
+//     Credentials({
+//       credentials: {
+//         email: {},
+//         password: {},
+//       },
+//       //@ts-ignore
+//       authorize: async (credentials) => {
+//         let user: UserType;
+
+//         try {
+//           user = await findUserByEmail(credentials.email as string);
+//         } catch (e) {
+//           console.log("error", e);
+//           throw new Error("Erro interno");
+//         }
+
+//         if (!user) throw new Error("Usuário ou senha inválidos.");
+
+//         if (credentials.password !== user.password) throw new Error("Usuário ou senha inválidos.");
+
+//         return user;
+//       },
+//     }),
+//   ],
+//   pages: {
+//     signIn: "/login",
+//   },
+// });
---
+++ src/next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth, { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      role: string
+      id?: string
+      name?: string | null
+      email?: string | null
+      image?: string | nulltring
+    }
+  }
+
+  interface User {
+    role: string // Include role in the User type
+  }
+}
