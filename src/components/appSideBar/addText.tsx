ea59443e5782366b7236f0fb354f0b8c9c842948
ml-vines <ml.vines.93@gmail.com>
2024-11-27 18:51:08 -0300

    fiz error when add more than one text

--- src/components/appSideBar/addText.tsx
+++ src/components/appSideBar/addText.tsx
@@ -3,27 +3,30 @@ import * as fabric from "fabric";
 
 export function AddText() {
     const { canvas } = useEditorContext();
+
+    const handleAddText = () => {
+      const textBox = new fabric.Textbox("Texto", {
+        width: 140,
+        fontSize: 60,
+        fill: "#a3c9a1",
+        lockSkewingX: true,
+        lockScalingFlip: true,
+        splitByGrapheme: true,
+      });
+    
+      textBox.controls.mt.visible = false;
+      textBox.controls.mb.visible = false;
+    
+      canvas?.add(textBox);
+      canvas?.centerObject(textBox);      
+    }
   
-    const textBox = new fabric.Textbox("Texto", {
-      width: 140,
-      fontSize: 60,
-      fill: "#a3c9a1",
-      lockSkewingX: true,
-      lockScalingFlip: true,
-      splitByGrapheme: true,
-    });
-  
-    textBox.controls.mt.visible = false;
-    textBox.controls.mb.visible = false;
-  
-    canvas?.centerObject(textBox);
+    
     return (
       <div className="flex gap-3 flex-col text-textForefround">
         <button
           type="button"
-          onClick={() => {
-            canvas?.add(textBox);
-          }}
+          onClick={handleAddText}
           className="border border-gray-300 rounded-lg px-2 py-1"
         >
           Texto teste
