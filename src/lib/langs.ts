bfcc647e19b62939b6ab2e482f291d3a4f4497de
Vinicius  Rangel <ml.vines.93@gmail.com>
2024-12-09 20:15:33 -0300

    email multi language solution after hours.....

--- src/lib/langs.ts
+++ src/lib/langs.ts
@@ -2,4 +2,13 @@ export const langs = [
     {name:'Portuguese',locale:'pt-BR',flag:'brasil.svg'},
     {name:'English',locale:'en',flag:'usa.svg'},
     {name:'Espanish',locale:'es',flag:'argentina.svg'},
-]
+] as const
+
+export type ILocaleLang = (typeof langs[number])["locale"];
+
+type DynamicLangMessage<T extends string> = Record<T, string | number | Record<string, any>>;
+
+export type EmailMessageLang = {
+    [key in ILocaleLang]: DynamicLangMessage<keyof Record<string,any>>;
+  };
+  
