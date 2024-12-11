26a1b09ccb2f810540d0f3a172266ccf7713940f
unknown <ml.vines.93@gmail.com>
2024-12-11 20:33:03 -0300

    update user in progress

---
+++ src/app/actions/updateUser.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { randomBytes } from "crypto";
+
+const schema = z.object({
+  name: z.string().min(1, { message: "O nome de usuário é obrigatório" }),
+  email: z.string().min(1, { message: "A senha é obrigatória" }),
+});
+
+export type AdminType = z.infer<typeof schema>;
+
+export async function updateUserAction(data: AdminType) {
+  const validatedFields = schema.safeParse(data);
+
+  if (!validatedFields.success) {
+    return {
+      zod_errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        role: {
+          connect: {
+            id: 1, //always register with a admin role
+          },
+        },
+      },
+    });
+    return {
+      message: "Usurário criado com sucesso",
+    };
+  } catch (e) {
+    console.log("Error register action", e);
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      switch (e.code) {
+        case "P2002":
+          if (e.meta && e.meta?.target === "users_email_key") {
+            return { error: "Este e-mail já está em uso." };
+          }
+          break;
+        default:
+          return {
+            error: "Erro ao criar usuário, por favor contate o suporte",
+          };
+      }
+    }
+
+    return {
+      error: "Erro ao criar usuário, por favor contate o suporte",
+    };
+  }
+}
