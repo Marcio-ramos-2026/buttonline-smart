9765cb8db4deb339f33db33083c408437e1ddfa9
Vinicius  Rangel <ml.vines.93@gmail.com>
2024-12-18 13:28:27 -0300

    model circle

--- src/components/editor/model.ts
+++ src/components/editor/model.ts
@@ -5,20 +5,10 @@ export type ModelType = {
     [key: string]: () => fabric.Object;  // The key is a string and the value is a function returning an object
   };
 export const getModels = (canvas: fabric.Canvas,model: editor_canvas):ModelType => {
-    const maxSize = 800
+    const maxSize = 800 // limite máximo do editor
     const canvasWidth = canvas.width || 0;
     const canvasHeight = canvas.height || 0;
-    const maxRadius = Math.min(canvasWidth, canvasHeight, maxSize) * 0.5; 
-
-    const shapes = Object.keys(model)
-    .filter(key => /^object_\d+$/.test(key)) // Match keys starting with "object_" followed by digits
-    .sort((a, b) => {
-        // Extract numeric parts for comparison
-        const numA = parseInt(a.split('_')[1], 10);
-        const numB = parseInt(b.split('_')[1], 10);
-        return numA - numB;
-    })
-    .map(key => model[key as keyof typeof model])
+    const maxRadius = Math.min(canvasWidth, canvasHeight, maxSize) * 0.5; //determina o max radius entre o limite máximo do editor e o tamanho da tela
 
 
     const centerX = canvas.width / 2;
@@ -29,9 +19,7 @@ export const getModels = (canvas: fabric.Canvas,model: editor_canvas):ModelType 
         'circle': () => {
 
             const createCircle = (radius:number,options: Partial<fabric.Circle> = {}) => {
-
-                const scaleFactor = maxRadius / Number(shapes[0]);
-                console.log('radius * scaleFactor',radius * scaleFactor)
+                const scaleFactor = maxRadius / Number(model.object_1);
                 return new fabric.Circle({
                     radius:radius * scaleFactor,
                     fill:'white',
@@ -48,9 +36,9 @@ export const getModels = (canvas: fabric.Canvas,model: editor_canvas):ModelType 
                 })
             }
           
-            const elements = [createCircle(Number(shapes[0]))];
-            elements.push(createCircle(Number(shapes[1]),{strokeDashArray:[5,5]}))
-            elements.push(createCircle(Number(shapes[2]),{strokeWidth:1}))
+            const elements = [createCircle(Number(model.object_1))];
+            elements.push(createCircle(Number(model.object_2),{strokeDashArray:[5,5]}))
+            elements.push(createCircle(Number(model.object_3),{strokeWidth:1}))
 
             return new fabric.Group(elements,{
                 selectable:false,
