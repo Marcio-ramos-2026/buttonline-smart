07afdedcc9e4cbb4fb0fae5a451e98b1934fe6a6
unknown <ml.vines.93@gmail.com>
2025-01-07 14:42:04 -0300

    images

---
+++ src/hooks/useInifiteScrollImage.ts
@@ -0,0 +1,64 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useIntersectionObserver } from "./useIntersectionObserver";
+
+type InfiniteScrollParams = {
+  endpoint: string;
+  limit: number;
+  get?: string;
+};
+
+export function useInifiteScrollImage({
+  endpoint,
+  limit,
+  get = "",
+}: InfiniteScrollParams) {
+  const [items, setItems] = useState<{ svg: string, id: string }[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { isIntersecting, ref } = useIntersectionObserver({ threshold: 1 });
+  const [hasMore, setHasMore] = useState(true);
+
+  const newItems = async () => {
+    setLoading(true);  
+
+    const response = await fetch(`${endpoint}?${get}&page=1&limit=${limit}`);
+
+
+    const { hits } = await response.json();
+
+    setItems(hits);
+    setPage(2);
+    // setHasMore(false);
+    setLoading(false);
+  };
+
+  const loadMoreItems = async () => {
+    setLoading(true);
+
+    const response = await fetch(
+      `${endpoint}?${get}&page=${page}&limit=${limit}`
+    );
+    const { hits } = await response.json();
+
+    setItems((prevItems) => [...prevItems, ...hits]);
+    setPage((prevPage) => prevPage + 1);
+    //setHasMore(data.length >= limit);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (isIntersecting && !loading && hasMore) {
+      loadMoreItems();
+    }
+  }, [isIntersecting, loading]);
+
+  useEffect(() => {
+    if (!loading) {
+      newItems();
+    }
+  }, [get]);
+
+  return { ref, items, loading };
+}
