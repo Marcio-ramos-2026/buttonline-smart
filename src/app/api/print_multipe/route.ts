7b018d2b3a23beff565cab7b56f52b2c5626f2cc
Vinicius  Rangel <vine.rangel93@gmail.com>
2025-03-13 14:04:29 -0300

    multiple fix

---
+++ src/app/api/print_multipe/route.ts
@@ -0,0 +1,115 @@
+import { createModel, pageSizes } from "@/components/editor/model";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { PDFDocument, Degrees, degrees, PDFImage } from "pdf-lib";
+import sharp from 'sharp'
+import { ModelConfig } from "@/components/editor/model";
+
+interface printRequest {
+  model_id: number;
+  svg: string;
+  dpi: number;
+  canvasWidth: number;
+  canvasHeight: number;
+}
+
+const mmToPt2 = (mm: number) => mm * 2.83465
+const mmToPt = (mm: number) => parseFloat((mm * 2.83465).toFixed(5));
+const mmToPx = (mm: number, dpi: number) => Math.round(mm * (dpi / 25.4))
+const pxToMm = (px: number, dpi: number) => px * 25.4 / dpi
+const pxToPt = (px: number, dpi: number) => px * (72/dpi)
+function pxToPt2(px:number) {
+  const pt = px * 0.75;
+  return pt;
+}
+
+export async function POST(request: NextRequest) {
+  const data: printRequest = await request.json()
+  const { model_id, svg, dpi, canvasHeight, canvasWidth } = data
+
+  const model = await prisma.editor_canvas.findFirst({
+    where: {
+      id: model_id
+    }
+  })
+
+  if (!model) {
+    return NextResponse.json(
+      { success: false, message: "Modelo não encontrando" },
+      { status: 400 }
+    );
+  }
+
+  const gabarito = model?.gabarito as ModelConfig["gabarito"]
+  const sizes = model.size?.split(",")
+  const orientation = gabarito.orientation ?? 'vertical'
+
+  let [modelWidth, modelHeight] = sizes as string[]
+  if (!modelHeight) modelHeight = modelWidth
+
+  const pageSize = pageSizes[gabarito.pdf] as [number, number]
+  if (orientation === 'horizontal' && pageSize[1] > pageSize[0] ) {
+    pageSize.reverse()
+  }
+
+  try {
+    // Create a new PDF document
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage(pageSize); // Define canvas size (width x height)
+    const { width, height } = page.getSize();
+
+    const pngBuffer = await sharp(Buffer.from(svg), { density: 1000 })
+      .png({ quality: 100 })
+      .toBuffer();
+    let pngImage = await pdfDoc.embedPng(pngBuffer);
+
+    
+    Object.values(gabarito.positions).forEach(async (p) => {
+      let imgWidth = mmToPt(parseFloat(modelWidth))
+      let imgHeight = mmToPt(parseFloat(modelHeight))
+
+      let x = mmToPt(p.x)
+      let y = height - mmToPt(parseFloat(modelHeight)) - mmToPt(p.y)
+
+      const rotate = p.rotate ?? 0
+
+      if (rotate == -90) {
+        y += imgHeight
+      }
+    
+
+      page.drawImage(pngImage, {
+        x: x,
+        y: y,
+        width: imgWidth,
+        height: imgHeight,
+        rotate: degrees(rotate)
+      });
+    });
+    
+    
+
+    // Set PDF metadata
+    pdfDoc.setTitle(model.name)
+    pdfDoc.setAuthor("Cardenas - Buttonline")
+    pdfDoc.setCreationDate(new Date())
+
+    // Serialize the PDF to bytes
+    const pdfBytes = await pdfDoc.save();
+
+    // Return the PDF as a response
+    return new NextResponse(pdfBytes, {
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename='${model.name}.pdf'`,
+      },
+    });
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+
+    return NextResponse.json(
+      { success: false, message: "Failed to generate PDF" },
+      { status: 500 }
+    );
+  }
+}
--- src/components/editor/multiple/multiple.tsx
+++ src/components/editor/multiple/multiple.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { CopyPlus, Trash2, Printer } from "lucide-react";
 import * as fabric from "fabric";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useEditorContext } from "@/context/editor";
 import { ReactSVG } from "react-svg";
 import {
@@ -18,12 +18,59 @@ type ButtonItem = {
   canvas: fabric.Canvas; // Se souber o tipo exato, substitua `any` pelo tipo correto
   qty: number;
   name?: string;
+  size: [string,string]
+  svg: string
 };
 
 export const MultipleButton = () => {
   const { canvas, currentModel } = useEditorContext();
-
+  
   const [buttons, setButtons] = useState<ButtonItem[]>([]);
+
+  const [printing, setPrinting] = useState(false);
+
+  useEffect(() => {
+    const storedButtons = localStorage.getItem("cardenas_multiple_buttons");
+    if (storedButtons) {
+      const parsedButtons = JSON.parse(storedButtons);
+
+      loadButtons(parsedButtons);
+    }
+  }, []);
+
+  
+  useEffect(() => {
+    if (buttons?.length) {
+      saveButtonsToLocalStorage(buttons);
+    }else{
+      localStorage.removeItem("cardenas_multiple_buttons")
+    }
+  }, [buttons]);
+
+  const saveButtonsToLocalStorage = (buttons: ButtonItem[]) => {
+    const serializedButtons = buttons.map((btn) => ({
+      ...btn,
+      canvasJSON: btn.canvas.toJSON(), // Convert canvas to JSON
+    }));
+    localStorage.setItem("cardenas_multiple_buttons", JSON.stringify(serializedButtons));
+  };
+
+  const loadButtons = (storedButtons: any[]) => {
+    const loadedButtons: ButtonItem[] = storedButtons.map((btn) => {
+      const canvasElement = document.createElement("canvas"); // Create an actual canvas element
+      const newCanvas = new fabric.Canvas(canvasElement); // Attach it to fabric.Canvas
+  
+      newCanvas.loadFromJSON(btn.canvasJSON, () => {
+        newCanvas.renderAll();
+      });
+  
+      return {
+        ...btn,
+        canvas: newCanvas,
+      };
+    });
+    setButtons(loadedButtons);
+  };
 
   const addMultiple = async () => {
     if (!canvas) return;
@@ -32,9 +79,66 @@ export const MultipleButton = () => {
       "cardenas_canvas",
     ]);
 
+    // canvasCopy.getObjects().forEach((obj) => {
+    //     if (obj.type === "image") {
+    //       const image = obj as fabric.FabricImage;
+    //       const imageElement = image.getElement() as HTMLImageElement;
+
+    //       const url = new URL(imageElement.src);
+    //       const pathname = url.pathname; // Get the file path
+    //       const extension = pathname.split(".").pop()?.toLowerCase(); // Extract the file extension (before query params)
+
+    //         // Check for file format based on the extension
+    //       const isJpeg = extension === "jpg" || extension === "jpeg";
+    //       // Convert the image to Base64
+    //       const base64 = image.toDataURL({
+    //         format: isJpeg ? "jpeg" : "png", // Use 'jpeg' if needed
+    //         quality: 1, // High quality for JPEG
+    //       });
+
+    //       imageElement.src = base64;
+
+    //       image.set({
+    //         element: imageElement,
+    //       });
+
+    //       obj = image
+    //     }
+    //   // Check if the object has the 'cardenas_canvas' property and is a group
+    //   if (!obj.cardenas_canvas) return;
+    
+    //   const canvas = obj as fabric.Group 
+    //   // Use forEachObject to iterate through the objects inside the group
+
+    //   canvas.getObjects().forEach((groupObj) => {
+    //     if(groupObj.cardenas_print) return
+
+    //     canvas.remove(groupObj)
+    //   });
+    // });
+
+    let sizes = currentModel?.size?.split(",") as [string, ...string[]];
+    if (!sizes[1]) sizes[1] = sizes[0];
+
+    const canvasWidth = fabric.util.parseUnit(`${sizes[0]}mm`);
+    const canvasHeight = fabric.util.parseUnit(`${sizes[1]}mm`);
+
+    const clip = new fabric.Group(canvasCopy.getObjects(),{});
+    const scaleFactor = parseFloat(sizes[0]) / Math.max(clip.width, clip.height);
+    clip.scale(scaleFactor)
+
+    const printCanvas = new fabric.Canvas("c", {
+      width: canvasWidth,
+      height: canvasHeight,
+    });
+
+    printCanvas.add(clip)
+    printCanvas.centerObject(clip)
+    printCanvas.clipPath = clip
+
     setButtons((current) => [
       ...current,
-      { canvas: canvasCopy, qty: 1, name: currentModel?.name },
+      { canvas: printCanvas, qty: 1, name: currentModel?.name, size: [sizes[0], sizes[1]], svg: "" },
     ]);
   };
 
@@ -54,12 +158,7 @@ export const MultipleButton = () => {
     <div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button
-            icon={<CopyPlus />}
-            variant={"outline"}
-            className="relative"
-            // onClick={addMultiple}
-          >
+          <Button icon={<CopyPlus />} variant={"outline"} className="relative">
             Multiple
             {buttons.length > 0 && (
               <div className="absolute -right-2 -top-2 h-6 w-6 bg-danger text-white font-bold rounded-full text-sm flex justify-center items-center">
@@ -80,50 +179,39 @@ export const MultipleButton = () => {
               Add current Button
             </Button>
 
-            <div className="max-h-80 overflow-y-auto ">
-              {buttons.map((item, key) => {
-                return (
-                  <div key={key}>
-                    <p className="text-xs">{item?.name}</p>
-                    <div className="flex items-center justify-center">
-                      <ReactSVG
-                        className=" text-red-500"
-                        src={`data:image/svg+xml;base64,${btoa(item.canvas.toSVG({ height: "40px", width: "40px" }))}`}
-                      />
-                      <div className="flex flex-col">
-                        <IncrementorInput
-                          min={1}
-                          defaultValue={item.qty}
-                          onChange={(e) =>
-                            changeQty(key, e.currentTarget.value)
-                          }
-                        />
-                      </div>
+            <div className="max-h-80 overflow-y-auto">
+              {buttons.map((item, key) => (
+                <div key={key}>
+                  <p className="text-xs">{item?.name}</p>
+                  <div className="flex items-center justify-center">
+                    <ReactSVG
+                      className="text-red-500 "
+                      src={`data:image/svg+xml;base64,${btoa(item.canvas.toSVG({ suppressPreamble: true }))}`}
 
-                      <Trash2
-                        height={30}
-                        width={30}
-                        onClick={() => removeMultiple(key)}
-                        color="red"
+
+                    />
+                    <div className="flex flex-col">
+                      <IncrementorInput
+                        min={1}
+                        defaultValue={item.qty}
+                        onChange={(e) =>
+                          changeQty(key, e.currentTarget.value)
+                        }
                       />
                     </div>
 
-                    <DividerHorizontal className="my-1" />
+                    <Trash2
+                      height={30}
+                      width={30}
+                      onClick={() => removeMultiple(key)}
+                      color="red"
+                    />
                   </div>
-                );
-              })}
-            </div>
 
-            {buttons.length > 0 && (
-              <Button
-                icon={<Printer />}
-                variant={"outline"}
-                className="relative mt-2"
-                onClick={addMultiple}
-              >
-                Print
-              </Button>
-            )}
+                  <DividerHorizontal className="my-1" />
+                </div>
+              ))}
+            </div>
           </div>
         </DropdownMenuContent>
       </DropdownMenu>
